881cdd926728cae7bbc1793e18d989788bae6475
Kwack Minseo <163567726+hometogo4ever@users.noreply.github.com>
2024-08-09 15:44:11 +0900

    버그 수정

--- EquipmentSystem/js/login.js
+++ EquipmentSystem/js/login.js
@@ -3,22 +3,25 @@ export function checkLoginStatus() {
       url: 'php/getUserId.php',
       type: 'POST',
       success: function(data) {
-          $('#container > header > #account').html(
-              `<p>환영합니다, ${data}}님!</p>
-              <ul>
-                  <li><a href="php/logout.php"><button id="logout">로그아웃</button></a></li>
-                  <li><a href="mypage.html"><button>마이페이지</button></a></li>
-              </ul>`);
-
+        if (!data) {
+            $('#container > header > #account').html(
+                `<p>로그인이 필요합니다.</p>
+                <ul>
+                    <li><a href="login.html"><button>로그인</button></a></li>
+                    <li><a href="join.html"><button>회원가입</button></a></li>
+                </ul>`
+            );
+        } else {
+            $('#container > header > #account').html(
+                `<p>환영합니다, ${data}님!</p>
+                <ul>
+                    <li><a href="php/logout.php"><button id="logout">로그아웃</button></a></li>
+                    <li><a href="mypage.html"><button>마이페이지</button></a></li>
+                </ul>`);
+        }
       },
       error: function() {
-          $('#container > header > #account').html(
-              `<p>로그인이 필요합니다.</p>
-              <ul>
-                  <li><a href="login.html"><button>로그인</button></a></li>
-                  <li><a href="join.html"><button>회원가입</button></a></li>
-              </ul>`
-          );
+          alert('로그인 상태 확인 실패');
       }
   });
 }
