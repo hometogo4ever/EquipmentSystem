a542decdb814b046a6d9e48ee5ff300e6124e69c
Kwack Minseo <163567726+hometogo4ever@users.noreply.github.com>
2024-08-10 22:31:06 +0900

    버그수정 2차

--- EquipmentSystem/php/getstatus.php
+++ EquipmentSystem/php/getstatus.php
@@ -9,8 +9,9 @@ if (mysqli_connect_errno()) {
     echo json_encode($arr);
 } else {
     $sql = "SELECT `equip`.`name` as `eqname`, `start_date`, `due_date`, `rent`.`status`, `pic_ref`, `equip`.`equip_id` as `eqid`   
-            FROM `rent`, `equip`
-            WHERE `rent`.`equip_id` = `equip`.`equip_id` AND `rent`.`user_id` = ?";
+        FROM `rent`
+        INNER JOIN `equip` ON `rent`.`equip_id` = `equip`.`equip_id`
+        WHERE `rent`.`user_id` = ?";
     $stmt = mysqli_prepare($link, $sql);
     $stmt->bind_param("s", $userid);
     $stmt->execute();
