789413044050696c6d438b538c7c6a60068add0d
Kwack Minseo <163567726+hometogo4ever@users.noreply.github.com>
2024-08-26 16:00:06 +0900

    Update getEqlist.php

--- EquipmentSystem/php/getEqlist.php
+++ EquipmentSystem/php/getEqlist.php
@@ -5,9 +5,9 @@ $link = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
 if (mysqli_connect_errno()) {
     echo "2";
 } else {
-    $sql = "SELECT `equip`.`name` as `eqname`, `maker`, `type`, `loc`.`name` as `locname`, `equip_status`, `pic_ref`, `feature`, `equip_id` as `eqid`
-             FROM `equip`, `loc`
-             WHERE `equip`.`loc_id` = `loc`.`loc_id`
+    $sql = "SELECT `equip`.`name` as `eqname`, `maker`, `kindof`.`name` as `tyname`, `loc`.`name` as `locname`, `equip_status`, `pic_ref`, `feature`, `equip_id` as `eqid`
+             FROM `equip`, `loc`, `kindof`
+             WHERE `equip`.`loc_id` = `loc`.`loc_id` AND `kindof`.`id` = `equip`.`type`
              LIMIT 10 OFFSET 0";
     $result = mysqli_query($link, $sql);
     $arr = array();
