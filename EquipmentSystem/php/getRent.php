601352bf74e26e59a750c806e2e7dbee1d886b51
Kwack Minseo <163567726+hometogo4ever@users.noreply.github.com>
2024-11-21 12:37:47 +0900

    추가 정보

--- EquipmentSystem/php/getRent.php
+++ EquipmentSystem/php/getRent.php
@@ -5,7 +5,7 @@ $link = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
 if (mysqli_connect_errno()) {
     echo "2";
 } else {
-    $sql = "SELECT `history`.`id` as `hisid`, `user`.`name` as `username`, `equip`.`name` as `eqname`, `history`.`start_date` as `date`
+    $sql = "SELECT `history`.`id` as `hisid`, `user`.`name` as `username`, `equip`.`name` as `eqname`, `history`.`start_date` as `date`, `history`.`end_date` as `date2`
              FROM `history`, `user`, `equip`
              WHERE `history`.`user_id` = `user`.`user_id` AND `history`.`equip_id` = `equip`.`equip_id`";
              
