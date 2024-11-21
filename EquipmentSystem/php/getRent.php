9527bae9c5fd3aa91b0546739b8298be76f59b94
Kwack Minseo <163567726+hometogo4ever@users.noreply.github.com>
2024-11-21 12:29:04 +0900

    장비 대여 기록 추적

---
+++ EquipmentSystem/php/getRent.php
@@ -0,0 +1,21 @@
+<?php
+
+include 'property.php';
+$link = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
+if (mysqli_connect_errno()) {
+    echo "2";
+} else {
+    $sql = "SELECT `history`.`id` as 'hisid`, `user`.`name` as `username`, `equip`.`name` as `eqname`, `history`.`start_date` as `date`
+             FROM `history`, `user`, `equip`
+             WHERE `history`.`user_id` = `user`.`user_id` AND `history`.`equip_id` = `equip`.`equip_id`";
+             
+    $result = mysqli_query($link, $sql);
+    $arr = array();
+    while ($row = mysqli_fetch_array($result)) {
+        array_push($arr, $row);
+    }
+    echo json_encode($arr);
+    mysqli_close($link);
+}
+
+?>
